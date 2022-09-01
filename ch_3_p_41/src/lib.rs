696f94883652650ce3ba03ba71fd40645a974a67
MykhayloChavarha <mishachavarha@gmail.com>
2022-09-01 20:32:21 +0300

    changed name

--- ch_3_p_41/src/lib.rs
+++ ch_3_p_41/src/lib.rs
@@ -6,7 +6,7 @@ pub struct MinCostB3Tree<const N: usize> {
     freq: [u64; N],
     sum_freq: [[u64; N]; N],
     opt_cost: [[u64; N]; N],
-    root: [[(u64,u64); N]; N]
+    root: [[(Option<u64>,Option<u64>); N]; N]
 }
 
 impl<const N: usize> MinCostB3Tree<N> {
@@ -15,7 +15,7 @@ impl<const N: usize> MinCostB3Tree<N> {
         for i in 0..N {
             sum_table[i][0] = freq[i];
             for j in 1..N {
-                sum_table[i][j] = sum_table[i][j-1];
+                sum_table[i][j] = sum_table[i][j-1] + freq[j];
             }
         }
         MinCostB3Tree {
@@ -23,7 +23,7 @@ impl<const N: usize> MinCostB3Tree<N> {
             freq,
             sum_freq: sum_table,
             opt_cost: [[0; N]; N],
-            root: [[(0,0); N]; N]
+            root: [[(None, None); N]; N]
         }
     }
 
@@ -36,34 +36,48 @@ impl<const N: usize> MinCostB3Tree<N> {
                 self.opt_cost[i][j] = self.calc_opt_cost(i,j);
             }
         }
-        self.opt_cost[0][N-1]
-        
+        self.opt_cost[0][N-1]    
     }
 
     fn calc_opt_cost(&mut self, n: usize, m: usize) -> u64 {
-        println!("({}, {})", n, m);
         if n > m {
             return 0; 
         } else if m == n {
+            self.root[n][m] = (Some(n as u64),Some(m as u64));
             return self.freq[m];
         } else {
             let mut min_cost = u64::MAX;
-            for i in n..=m {
-                let mut first = 0;
-                if i > n {
-                    first = self.opt_cost[n][i-1];
-                }
+            let mut first_root = 0; 
+            let mut second_root = 0; 
+            // n <= i <=j <= m 
+            for j in n..=m {
+                for i in n..=j {
+                    let mut first = 0; 
+                    let mut second = 0; 
+                    let mut third = 0;
 
-                let mut second = 0; 
-                if i < m {
-                    second = self.opt_cost[n][i+1];
-                }
-                let cost = self.sum_freq[n][m] + first + second;
-                if cost < min_cost {
-                    min_cost = cost
-                }
+                    if i > n {
+                        first = self.opt_cost[n][i-1];
+                    }
+
+                    if i < m && j > n {
+                        second = self.opt_cost[i+1][j-1];
+                    }
+
+                    if j < m {
+                        third = self.opt_cost[j+1][m];
+                    }
+
+                    let cost = self.sum_freq[n][m] + first + second + third;
+
+                    if cost < min_cost {
+                        min_cost = cost;
+                        first_root = i;
+                        second_root = j;
+                    }
+                }  
             }
-            println!("{}", min_cost);
+            self.root[n][m] = (Some(first_root as u64),Some(second_root as u64));
             return min_cost;
         }
     }
@@ -76,13 +90,15 @@ mod tests {
 
     #[test]
     fn ten_elements() {
-        const size: usize = 5;
+        const size: usize = 6;
 
-        let keys:[u64; size] = [0,1,2,3,4];
-        let values:[u64; size] = [2,4,6,8,100];
+        let keys:[u64; size] = [0,1,2,3,4,5];
+        let values:[u64; size] = [1,4,6,8,10,100];
         // println!("{:?}", MinCostB3Tree::<size>::new(keys, values));
         let mut b_tree = MinCostB3Tree::new(keys,values);
         println!("Min Cost B3Tree: {:?}", b_tree.calculate());
+        println!("sum: {:?}", b_tree.sum_freq);
         println!("table: {:?}", b_tree.opt_cost);
+        println!("root: {:?}", b_tree.root);
     }
 }
