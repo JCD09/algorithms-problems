c28bfeb83f683e8f3495b0d57bbab65ad2a885cd
MykhayloChavarha <mishachavarha@gmail.com>
2022-09-01 18:04:38 +0300

    edited gitignore

--- b-tree/target/debug/deps/b_tree-6b9755dbbb356c14.d
+++
@@ -1,5 +0,0 @@
-/home/mish/Projects/algorithms-problems/target/debug/deps/b_tree-6b9755dbbb356c14.rmeta: b-tree/src/lib.rs
-
-/home/mish/Projects/algorithms-problems/target/debug/deps/b_tree-6b9755dbbb356c14.d: b-tree/src/lib.rs
-
-b-tree/src/lib.rs:
